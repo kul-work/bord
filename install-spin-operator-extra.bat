61d79b95419199cb744e21e549490c37a32bd4ce
Mihaita Nita <mihaita.nita@gmail.com>
2025-11-26 22:22:05 +0200

    clean-up

--- install-spin-operator-extra.bat
+++ install-spin-operator-extra.bat
@@ -1,5 +1,3 @@
 rem adding SPIN containerd extra flags
 docker cp ./.k3d/config.toml.tmpl k3d-bord-server-0:/var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl
 docker restart k3d-bord-server-0
-rem fix registry networking
-rem docker network connect k3d-bord k3d-bord-registry
